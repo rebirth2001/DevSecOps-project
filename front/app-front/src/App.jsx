06edc48766d116234dacdf1fe5c766bec75be166
zamameayoub1 <zamame.ayoub@gmail.com>
2023-10-26 23:20:02 +0100

    welcom page

--- front/app-front/src/App.jsx
+++ front/app-front/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './component/navbar'
+import Home from './pages/home'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Home></Home>
     </>
   )
 }
