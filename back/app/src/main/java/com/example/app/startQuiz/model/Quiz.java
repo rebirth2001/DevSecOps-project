659f0d6d0a1feef91bc29a2f28cca46d64a925f0
joui5 <abdelwadoud.joui@edu.uca.ma>
2023-12-08 01:29:24 +0100

    participant

--- back/app/src/main/java/com/example/app/startQuiz/model/Quiz.java
+++ back/app/src/main/java/com/example/app/startQuiz/model/Quiz.java
@@ -1,5 +1,6 @@
 package com.example.app.startQuiz.model;
 
+import com.example.app.participant.model.Participant;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +19,10 @@ public class Quiz {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "quiz")
     private List<Question> questions;
-
+    private String codeOfInvitation;
     private String title;
     private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "quiz")
+    private List<com.example.app.participant.model.Participant> participant;
 }
